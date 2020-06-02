bc96d351fd041f72e95e48320e26171b2d1e4006
Alejandro Figueroa <alexphi@gmail.com>
2020-06-02 14:23:43 -0500

    Added cleanup

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # alejof-thumbnailer
 
-Azure Functions App to generate "smart" thumbnails for uploaded images.
+Azure Functions App to generate "smart" thumbnails for uploaded images, using the Azure [Computer Vision API][vision-api].
+
+[vision-api]:https://azure.microsoft.com/en-us/services/cognitive-services/computer-vision/
 
 # Functions
 
-There are two functions in this Azure Functions app, one that generates "smart" thumbnails for images, using the  [Computer Vision API][vision-api], an another one that scales down images if they are too big (because the computer vision api fails with big images).
+**Thumbnail**: Function that generates "smart" thumbnails for images using the Vision API. It defaults to a 150x150 thumbnail size, unless otherwise specified in the Application Settings.
 
-The functions are triggered by queue messages containing the full path of an image blob in Blob Storage.
+**Resize**: Function that scales down images if they are too big. This is because the computer vision api may fail with big images, so this function pre-processes imags that are wider or higher than the maximum media size (defauts to 1200px, also overriden by App Settings).
 
-[vision-api]:https://azure.microsoft.com/en-us/services/cognitive-services/computer-vision/
+# Processing Flow
+
+![flow](./docs/flow.png)
+
+The "main" **Thumbnail** function is triggered by a simple string message in a `media-thumbnail-signal` queue, which contains the full path of an image blob in Blob Storage (in the form of `{container}/{blob name}`).
+
+If the image is bigger that the maximum configured size, a new message is put on a `media-resize-signal` and it gets picked up by the **Resize** function, which creates a _new blob_ for the resized image. Next, a new message is put on the thumbnail signal queue (with the new blob's path) to start a new flow.
--- src/Functions.cs
+++ src/Functions.cs
@@ -11,6 +11,7 @@ namespace AlejoF.Thumbnailer
     {
         private const string ThumbnailSignalQueue = "media-thumbnail-signal";
         private const string ResizeSignalQueue = "media-resize-signal";
+        private const string CleanupSignalQueue = "media-cleanup-signal";
         private const string ResizedNameToken = "__resized__";
         public const string StorageConnectionString = "StorageConnectionString";
 
@@ -25,7 +26,8 @@ namespace AlejoF.Thumbnailer
         public async Task Thumbnail(
             [QueueTrigger(ThumbnailSignalQueue, Connection = StorageConnectionString)]string blobName,
             [Blob("{queueTrigger}", FileAccess.Read, Connection = StorageConnectionString)] Stream input,
-            [Queue(ResizeSignalQueue, Connection = StorageConnectionString)]IAsyncCollector<string> queueCollector,
+            [Queue(ResizeSignalQueue, Connection = StorageConnectionString)]IAsyncCollector<string> resizeQueueCollector,
+            [Queue(CleanupSignalQueue, Connection = StorageConnectionString)]IAsyncCollector<string> cleanupQueueCollector,
             ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {blobName}");
@@ -33,8 +35,10 @@ namespace AlejoF.Thumbnailer
             var extension = System.IO.Path.GetExtension(blobName);
             var outputName = blobName.Remove(blobName.Length - extension.Length) + "-thumb" + extension;
 
-            // fix name for resized images
-            outputName = outputName.Replace(ResizedNameToken, string.Empty);
+            // Cleanup name and blob for resized images
+            var wasResized = blobName.Contains(ResizedNameToken);
+            if (wasResized)
+                outputName = outputName.Replace(ResizedNameToken, string.Empty);
 
             var result = await new Transforms.Thumbnail(settings)
                 .Execute(input, blobName, outputName);
@@ -42,7 +46,13 @@ namespace AlejoF.Thumbnailer
             if (!result.Success)
             {
                 log.LogInformation(result.Message);
-                await queueCollector.AddAsync(blobName);
+                await resizeQueueCollector.AddAsync(blobName);
+            }
+
+            if (wasResized)
+            {
+                log.LogInformation(result.Message);
+                await cleanupQueueCollector.AddAsync(blobName);
             }
         }
 
@@ -66,6 +76,18 @@ namespace AlejoF.Thumbnailer
                 log.LogInformation($"Re-queuing for thumbnail generation: {outputName}");
                 await queueCollector.AddAsync(outputName);
             }
+        }
+
+        [FunctionName(nameof(Cleanup))]
+        public async Task Cleanup(
+            [QueueTrigger(CleanupSignalQueue, Connection = StorageConnectionString)]string blobName,
+            ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {blobName}");
+
+            var blockBlob = Helpers.BlobHelper.GetBlobReference(blobName);
+            if (blockBlob != null)
+                await blockBlob.DeleteIfExistsAsync();
         }
     }
 }
